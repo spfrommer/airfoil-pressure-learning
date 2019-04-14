788d72e273ac515cd8d0ea554231f9ab2fbe69f9
Samuel <Samuel Pfrommer>
2019-04-14 14:17:41 -0400

    Refactoring

--- airsim/flow_images/train.py
+++ airsim/flow_images/train.py
@@ -1,16 +1,9 @@
-import torch.multiprocessing as mp
-try:
-    mp.set_start_method('spawn')
-except RuntimeError:
-    pass
-
 import sys
 import os
 import os.path as op
 path = op.dirname(op.dirname(op.dirname(op.abspath(__file__))))
 print('Setting project root path: ' + path)
 sys.path.append(path)
-
 
 import time
 import torch
@@ -35,67 +28,83 @@ sdf_samples = False
 test_trained_net = True
 load_net = False
 net_path = dirs.out_path('trained', 'net.pth')
-epochs = 300
+epochs = 2
 batch_size = 64
 learning_rate = 0.0001
 
-train_dataset = ProcessedAirfoilDataset(
-        dirs.out_path('processed', 'train'), sdf_samples, device)
-train_loader = DataLoader(train_dataset, batch_size=batch_size,
-                          shuffle=True, num_workers=2)
-test_dataset = ProcessedAirfoilDataset(
-        dirs.out_path('processed', 'test'), sdf_samples, device)
-test_loader = DataLoader(test_dataset, batch_size=batch_size,
-                          shuffle=False, num_workers=2)
+def main():
+    #setup_multiprocessing()
+    train_dataset, train_loader, test_dataset, test_loader = init_data()
+    net = GuoCNN().to(device)
+    if load_net:
+        net.load_state_dict(torch.load(net_path))
+    loss = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
 
-info_logger.info("Training dataset size: {}".format(len(train_dataset)))
-info_logger.info("Testing dataset size: {}".format(len(test_dataset)))
+    training_start_time = time.time()
+    try:
+        train(net, optimizer, loss, train_loader, test_loader)
+    except Exception:
+        info_logger.exception("Error in training")
+    info_logger.info("Training finished, took {:.2f}s".format(time.time() - training_start_time))
 
-net = GuoCNN().to(device)
-if load_net:
-    net.load_state_dict(torch.load(net_path))
+    if test_trained_net:
+        test_net(net, test_dataset[0])
 
-loss = torch.nn.MSELoss()
+def setup_multiprocessing():
+    import torch.multiprocessing as mp
+    mp.set_start_method('spawn')
+    #try:
+        #mp.set_start_method('spawn')
+    #except RuntimeError:
+        #pass
 
-optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate)
+def init_data():
+    train_dataset = ProcessedAirfoilDataset(
+            dirs.out_path('processed', 'train'), sdf_samples, device)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size,
+                              shuffle=True, num_workers=0)
+    test_dataset = ProcessedAirfoilDataset(
+            dirs.out_path('processed', 'test'), sdf_samples, device)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size,
+                              shuffle=False, num_workers=0)
+    info_logger.info("Training dataset size: {}".format(len(train_dataset)))
+    info_logger.info("Testing dataset size: {}".format(len(test_dataset)))
+    return train_dataset, train_loader, test_dataset, test_loader
 
-batches = len(train_loader)
-training_start_time = time.time()
-info_logger.info("Got {} batches".format(batches))
-
-try:
+def train(net, optimizer, loss, train_loader, test_loader):
+    batches = len(train_loader)
+    info_logger.info("Got {} batches".format(batches))
     for epoch in range(epochs):
         running_loss = 0.0
-        print_every = 5
+        print_every = 1
         start_time = time.time()
         total_train_loss = 0
         
+        # Go through entire training dataset
         for i, data in enumerate(train_loader, 0):
             airfoils, pressures = data
             airfoils, pressures = Variable(airfoils), Variable(pressures)
             
             optimizer.zero_grad()
             
-            #Forward pass, backward pass, optimize
             pressures_pred = net(airfoils)
             loss_size = loss(pressures_pred, pressures)
             loss_size.backward()
             optimizer.step()
             
-            #Print statistics
             running_loss += loss_size.item()
             total_train_loss += loss_size.item()
 
             #Print every nth batch of an epoch
             if (i + 1) % (print_every + 1) == 0:
                 info_logger.info("Epoch {}, {:d}% \t train loss: {:.2f} took: {:.2f}s".format(
-                        epoch+1, int(100 * (i+1) / batches), running_loss / float(print_every * batch_size),
+                        epoch+1, int(100 * (i+1) / batches), running_loss / (print_every * batch_size),
                         time.time() - start_time))
-                #Reset running loss and time
                 running_loss = 0.0
                 start_time = time.time()
             
-        #At the end of the epoch, do a pass on the test set
+        # At the end of the epoch, do a pass on the test set
         total_test_loss = 0
         for airfoils, pressures in test_loader:
             airfoils, pressures = Variable(airfoils), Variable(pressures)
@@ -104,20 +113,18 @@ try:
             loss_size = loss(pressures_pred, pressures)
             total_test_loss += loss_size.item()
            
-        avg_train_loss = total_train_loss / len(train_dataset)
-        avg_test_loss = total_test_loss / len(test_dataset)
+        avg_train_loss = total_train_loss / (len(train_loader) * batch_size)
+        avg_test_loss = total_test_loss / (len(test_loader) * batch_size)
+ 
         info_logger.info("Finished epoch {}".format(epoch+1))
         info_logger.info("Train loss = {:.2f}".format(avg_train_loss))
         info_logger.info("Test loss = {:.2f}".format(avg_test_loss))
         data_logger.info("{}, {}, {}".format(epoch, avg_train_loss, avg_test_loss))
         torch.save(net.state_dict(), net_path)
-except Exception:
-    info_logger.exception("Error in training")
-    
-info_logger.info("Training finished, took {:.2f}s".format(time.time() - training_start_time))
 
-if test_trained_net:
-    airfoil, pressure = test_dataset[0]
+
+def test_net(net, sample):
+    airfoil, pressure = sample
     airfoil, pressure = Variable(airfoil), Variable(pressure)
     airfoil = airfoil.view(1, 1, 256, 256)
     pressure = pressure.view(1, 1, 256, 256)
@@ -138,3 +145,5 @@ if test_trained_net:
     info_logger.info(pressure_error)
     pressure_error = (pressure_error / 200) + 0.5
     utils.save_image(pressure_error, 'pressure_error.png')
+
+if __name__ == "__main__":main()
